aadd4611ee6cb9c47d0d06367d4c00f220ba6ea2
haz10em <alyaarihazem@gmail.com>
2025-02-07 01:24:45 +0300

    added page page-not-found and change the footer and import found awsome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,21 +7,24 @@ import { AuthGuard } from './core/guards/auth.guard';
 import { ShippingComponent } from './components/checkout/shipping/shipping.component';
 import { PaymentComponent } from './components/checkout/payment/payment.component';
 import { ReviewComponent } from './components/checkout/review/review.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: ProductListComponent },
+  { path: '', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailComponent },
   { path: 'cart', component: CartComponent },
-  { 
-    path: 'checkout', 
+
+  {
+    path: 'checkout',
     component: CheckoutComponent,
+    // canActivate: [AuthGuard],
     children: [
       { path: '', redirectTo: 'shipping', pathMatch: 'full' },
       { path: 'shipping', component: ShippingComponent },
       { path: 'payment', component: PaymentComponent },
       { path: 'review', component: ReviewComponent },
-      { path: '', redirectTo: '/shipping', pathMatch: 'full' },
-    ]
+    ],
   },
-  { path: '', redirectTo: '', pathMatch: 'full' },
+
+  { path: '**', component:PageNotFoundComponent},
 ];
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,20 @@
-import { CanActivate, CanActivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const isLoggedIn = localStorage.getItem('user');
+    const isLoggedIn = !!localStorage.getItem('user');
+
     if (!isLoggedIn) {
       this.router.navigate(['/']);
       return false;
     }
+
     return true;
   }
 }
